69297857f312e0ee1126b82b215a0a91ec350f67
John Fisk <70089538+johnfisk548@users.noreply.github.com>
2022-07-08 09:17:15 +0530

    Revert "Update __main__.py"
    This reverts commit 70981e237383d9aeecf40d81aa547b38d92b5032.

--- bot/__main__.py
+++ bot/__main__.py
@@ -44,18 +44,18 @@ def stats(update, context):
     mem_t = get_readable_file_size(memory.total)
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
-    stats = f'<b>Commit Date:</b> {last_commit}\n\n'\
+    stats = f'<b>Commit Date:</b> {last_commit}\n'\
             f'<b>Bot Uptime:</b> {currentTime}\n'\
-            f'<b>OS Uptime:</b> {osUptime}\n\n'\
+            f'<b>OS Uptime:</b> {osUptime}\n'\
             f'<b>Total Disk Space:</b> {total}\n'\
-            f'<b>Used:</b> {used} | <b>Free:</b> {free}\n\n'\
+            f'<b>Used:</b> {used} | <b>Free:</b> {free}\n'\
             f'<b>Upload:</b> {sent}\n'\
-            f'<b>Download:</b> {recv}\n\n'\
+            f'<b>Download:</b> {recv}\n'\
             f'<b>CPU:</b> {cpuUsage}%\n'\
             f'<b>RAM:</b> {mem_p}%\n'\
-            f'<b>DISK:</b> {disk}%\n\n'\
+            f'<b>DISK:</b> {disk}%\n'\
             f'<b>Physical Cores:</b> {p_core}\n'\
-            f'<b>Total Cores:</b> {t_core}\n\n'\
+            f'<b>Total Cores:</b> {t_core}\n'\
             f'<b>SWAP:</b> {swap_t} | <b>Used:</b> {swap_p}%\n'\
             f'<b>Memory Total:</b> {mem_t}\n'\
             f'<b>Memory Free:</b> {mem_a}\n'\
